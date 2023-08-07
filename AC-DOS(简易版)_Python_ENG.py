2cfab5d68d917c9349a3c6ede4b54c6623dacd15
WSx32 <scxz0886521_wsx32@outlook.com>
2023-08-07 15:25:10 +0800

    Add files via upload

---
+++ AC-DOS(简易版)_Python_ENG.py
@@ -0,0 +1,25 @@
+print("======================================")
+print("           AC-DOS 1.1 Beta\nBased on Python 3.11.1 64-bit on AMD64")
+print("======================================")
+print("Loading sys files...")
+import time
+print("Loading Math Support...")
+import math
+print("Loading Easy Gui 0.98.2 Release...")
+import easygui
+print("Loading PyGame 2.5.0...")
+import pygame
+print("Loading PyMunk 6.5.1...")
+import pymunk
+print("Loading OpenCV 4.8.0.74 and Numpy 1.25.2...")
+import cv2
+print("chkdisk.exe is checking disk...")
+print("Checking disk space...")
+print("Checking free space...")
+time.sleep(2)
+print("10240MB RAM OK. ")
+time.sleep(3)
+print("Enter quit() to exit. ")
+while True:
+    i=input("AMD64$/usr/admin~%>")
+    eval(i)
